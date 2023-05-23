fc6021f326875b124b0775ebd7e452ca39b9ab86
yisuseas <yisuseas@gmail.com>
2023-05-22 19:25:27 -0600

    Profile no longer exists

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,16 +1,5 @@
 <nav>
-	<ul>
-		<li>
-			<strong>
-				<a href="/"> Chat Rooms </a>
-			</strong>
-		</li>
-	</ul>
-	<ul>
-		<li>
-			<strong>
-				<a href="/profile"> Profile </a>
-			</strong>
-		</li>
-	</ul>
+	<a href="/">
+		<strong> Chat Rooms </strong>
+	</a>
 </nav>
