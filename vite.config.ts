ecc80fb0a08694f68a6d16a8f9188f705159a419
yisuseas <yisuseas@gmail.com>
2023-05-22 23:43:33 -0600

    Server options

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const options = {
+	strictPort: true,
+	port: 5173
+};
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: options,
+	preview: options
 });
