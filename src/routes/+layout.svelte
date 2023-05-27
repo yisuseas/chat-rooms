c868596625e8a1e9d2f32b48dba943191fece27b
yisuseas <yisuseas@gmail.com>
2023-05-27 09:26:30 -0600

    Chat list

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,103 @@
 <script lang="ts">
-	import Navbar from '$lib/components/Navbar.svelte';
-	import Nuke from '$lib/components/Nuke.svelte';
+	import { page } from '$app/stores';
 	import '$lib/styles/global.scss';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 </script>
 
-<Navbar />
-<details>
-	<summary> Your rooms: </summary>
+<slot />
+<div class:not-home={$page.url.pathname !== '/'}>
+	<h2>
+		{#if data.userRooms.length > 0}
+			YOUR CHAT ROOMS
+		{:else}
+			YOU HAVE NO ROOMS
+		{/if}
+	</h2>
 	<ul>
-		{#each data.userRooms as { id, title } (id)}
-			<li>
-				<a href="/{id}">{title}</a>
+		{#each data.userRooms as room}
+			{@const roomPath = '/' + room.id}
+			<li class:active={$page.url.pathname === roomPath}>
+				<a href={roomPath}>
+					{room.title}
+				</a>
 			</li>
 		{/each}
 	</ul>
-</details>
-<slot />
-<Nuke />
+</div>
+
+<style lang="scss">
+	div {
+		@extend %card;
+		display: flex;
+		text-align: center;
+		flex-direction: column;
+
+		&.not-home {
+			display: none;
+			@include screen-lg {
+				display: flex;
+			}
+		}
+	}
+
+	h2 {
+		font-family: $font-display;
+		line-height: $box-height;
+	}
+
+	ul {
+		z-index: 1;
+		display: flex;
+		flex-direction: column;
+	}
+
+	li {
+		box-sizing: content-box;
+		height: $box-height;
+		border-color: $dark-accent;
+		position: relative;
+		@include light-theme {
+			border-color: $light-accent;
+		}
+
+		&:not(:last-child) {
+			border-bottom-width: 1px;
+		}
+	}
+
+	.active {
+		color: $dark-background;
+		@include light-theme {
+			color: $light-text;
+		}
+
+		&::before {
+			content: '';
+			display: block;
+			z-index: -1;
+			width: calc(100% + 2rem);
+			height: calc($box-height + 2px);
+			position: absolute;
+			top: -1px;
+			left: -1rem;
+			background-color: $primary;
+			border-top-width: 1px;
+			border-bottom-width: 1px;
+
+			border-color: $dark-text;
+			@include light-theme {
+				border-color: $light-text;
+			}
+		}
+	}
+
+	a {
+		display: inline-flex;
+		place-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
